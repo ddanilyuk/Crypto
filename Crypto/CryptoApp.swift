d8d3a103273a2d44819ac2d7ee78cbbe7530150d
denis.danyliuk <danis.danilyuk@gmail.com>
2022-07-17 00:16:11 +0300

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Denys Danyliuk on 17.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
